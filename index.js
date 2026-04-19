a99d18476f8076383a7930c9059dc44367697474
WEB-SAYT-YARATISH <152644806+sayt-yaratish@users.noreply.github.com>
2026-04-19 11:38:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -105,6 +105,21 @@ ${orderText}
         })
     });
 
+    document.addEventListener('touchstart', function (event) {
+  if (event.touches.length > 1) {
+    event.preventDefault();
+  }
+}, { passive: false });
+
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  let now = new Date().getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
+
     alert("Заказ отправлен ✅");
     clearCart();
 }
